283d63398005d07ff946591b252c7088108e6168
DuongCoder <gduongit9@gmail.com>
2021-03-24 18:38:29 +0700

    bai4

--- src/main/java/DiamonShop/UserController/HomeController.java
+++ src/main/java/DiamonShop/UserController/HomeController.java
@@ -1,14 +1,21 @@
 package DiamonShop.UserController;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import DiamonShop.Dao.HomeDao;
+
 @Controller
 public class HomeController {
+	@Autowired
+	HomeDao HomeDao;
+	
 	@RequestMapping(value = {"/","/trang-chu"})
 	public ModelAndView Index() {
 		ModelAndView mv = new ModelAndView("user/index");
+		mv.addObject("slides",HomeDao.GetDataSlide());
 		return mv;
 	}
 	
